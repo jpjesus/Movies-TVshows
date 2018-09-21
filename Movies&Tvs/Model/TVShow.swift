26a7ad25fa1913fa2f3bb3610a22597f72a99775
Jesus Alberto Parada Duque <jesus.parada@grability.com>
2018-09-21 17:19:30 -0500

    Adding model

---
+++ Movies&Tvs/Model/TVShow.swift
@@ -0,0 +1,42 @@
+//
+//  TVShow.swift
+//  Movies&Tvs
+//
+//  Created by Jesus on 9/21/18.
+//  Copyright © 2018 Jesus Paraada. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+final class TVShow: Program, Mappable {
+    var id: String?
+    var video: Bool?
+    var title: String?
+    var posterPath: String?
+    var isForAdult: Bool?
+    var overview: String?
+    var genreIDs: [Int]?
+    var averageVote: Double?
+    var popularity: Double?
+    var originalTitle: String?
+    var originCountry: [String]?
+    var airDate: Date?
+    
+    init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        video <- map["video"]
+        posterPath <- map["poster_path"]
+        isForAdult <- map["adult"]
+        overview <- map["overview"]
+        airDate <- map["first_air_date"]
+        genreIDs <- map["genre_ids"]
+        averageVote <- map["vote_average"]
+        originalTitle <- map["original_title"]
+        popularity <- map["popularity"]
+        originCountry <- map["origin_country"]
+    }
+}
---
+++ Podfile
@@ -0,0 +1,28 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Movies&Tvs' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Movies&Tvs
+  pod 'Kingfisher'
+  pod 'SwiftyHelpers'
+  pod 'RxDataSources'
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'Moya/RxSwift'
+  pod 'Moya-ObjectMapper/RxSwift'
+  pod 'SwiftSpinner'
+
+  target 'Movies&TvsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Movies&TvsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
